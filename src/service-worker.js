7c661ba6a27c44accd9c3ce8672ba7870b6a6a09
Angelo <angelorj97@hotmail.com>
2023-06-25 19:54:25 -0400

    Fixed PWA

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,12 +1,17 @@
 self.addEventListener("install", e => {
 	e.waitUntil(
 		caches.open("static").then(cache => {
-			return cache.addAll(["./", "./icons/manifest-icon-192.maskable.png"]);
+			return cache.addAll(["/"]);
 		})
 	);
 });
 
+self.addEventListener("activate", e => {
+	//console.log(`Intercepting fetch request for: ${e.request.url}`);
+	// console.log("sw has been activated");
+});
+
 self.addEventListener("fetch", e => {
 	//console.log(`Intercepting fetch request for: ${e.request.url}`);
-	console.log("fetching");
+	
 });
